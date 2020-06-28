2ac124859b4399bc074d4d56cc1f98d78aaa6699
mishrun <mishrun.git@gmail.com>
2020-06-28 23:23:30 +0900

    delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-curl -sS https://getcomposer.org/installer | php
-php composer.phar install
-php composer.phar require --dev phpunit/phpunit ^9
-
-# PHPUnitインストール
-./vendor/bin/phpunit tests
-php composer.phar dump-autoload
